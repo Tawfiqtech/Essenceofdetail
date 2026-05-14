6d454cd2de3ff1a96c7d3a7a3eb637889b674dc5
admin <admin@admins-iMac.local>
2026-05-13 17:54:47 -0700

    static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-    ],
-  },
+  output: 'export'
 }
 
 export default nextConfig
